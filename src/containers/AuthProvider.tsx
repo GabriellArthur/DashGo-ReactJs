97150545d41bc92fbd294618a41368e6d5b5099d
Gabriel Fiusa <gabriel.arthur2@gmail.com>
2022-05-30 02:12:35 -0300

    ++

--- src/containers/AuthProvider.tsx
+++ src/containers/AuthProvider.tsx
@@ -9,80 +9,78 @@ import { AuthContext, SignInCredentials } from "../contexts/Auth";
 import { api } from "../services/api";
 
 interface AuthProviderProps {
-  children: ReactNode;
+   children: ReactNode;
 }
 
 interface User {
-  id: number;
-  email: string;
-  name: string;
-  createdAt: string;
-  token: string;
+   id: number;
+   email: string;
+   name: string;
+   createdAt: string;
+   token: string;
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<User | null>(null);
-  const isAuthenticated = !!user;
+   const [user, setUser] = useState<User | null>(null);
+   const isAuthenticated = !!user;
 
-  const router = useRouter();
-  const toast = useToast();
+   const router = useRouter();
+   const toast = useToast();
 
-  async function signIn(credentials: SignInCredentials) {
-    try {
-      const { data } = await api.post("sessions", credentials);
+   async function signIn(credentials: SignInCredentials) {
+      try {
+         const { data } = await api.post("/sessions", credentials);
 
-      setUser({
-        id: data.id,
-        email: data.email,
-        createdAt: data.createdAt,
-        name: data.name,
-        token: data.token
-      });
+         setUser({
+            id: data.id,
+            email: data.email,
+            createdAt: data.createdAt,
+            name: data.name,
+            token: data.token
+         });
 
-      setCookie(undefined, "@dashgo.token", data.token);
+         setCookie(undefined, "@dashgo.token", data.token);
 
-      router.push("/dashboard");
-    } catch (error) {}
-  }
+         router.push("/dashboard");
+      } catch (error) { }
+   }
 
-  function signOut() {
-    destroyCookie(undefined, "@dashgo.token");    
+   function signOut() {
+      destroyCookie(undefined, "@dashgo.token");
 
-    router.push("/");
-  }
-
-  useEffect(() => {
-    const cookies = parseCookies();
-    const token = cookies["@dashgo.token"];
-
-    if (token) {
-      api
-        .get("/me", { headers: { Authorization: `Bearer ${token}` } })
-        .then(response => {          
-          setUser({
-            ...response.data,
-            token
-          });
-
-          if (router.asPath === "/") {
-            router.push("/dashboard");
-          }          
-        });
-    } else {
       router.push("/");
-    }
-  }, []);
+   }
 
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        isAuthenticated,
-        signIn,
-        signOut
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+   useEffect(() => {
+      const cookies = parseCookies();
+      const token = cookies["@dashgo.token"];
+
+      if (token) {
+         api
+            .get("/me", { headers: { Authorization: `Bearer ${token}` } })
+            .then(response => {
+               setUser({
+                  ...response.data,
+                  token
+               });
+
+               if (router.asPath === "/") {
+                  router.push("/dashboard");
+               }
+            });
+      }
+   }, []);
+
+   return (
+      <AuthContext.Provider
+         value={{
+            user,
+            isAuthenticated,
+            signIn,
+            signOut
+         }}
+      >
+         {children}
+      </AuthContext.Provider>
+   );
 }
